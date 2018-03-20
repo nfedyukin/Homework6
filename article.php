9fda22518dbd11829174e068235a1066114c8612
Nikolay Fedyukin <nick_fed@mail.ru>
2018-03-21 00:39:00 +0300

    bugs corrections 20.03

--- article.php
+++ article.php
@@ -8,7 +8,7 @@ if(isset($_GET['id']))
 {
     $query = 'SELECT * FROM news WHERE id = :id';
     $db = new DB;
-    $view ->add('newslist', $db->query($query,[':id'=>$_GET['id']],\News\Article::class));
+    $view->add('newslist', $db->query($query,[':id'=>$_GET['id']],\News\Article::class));
     $view->display(__DIR__ . '/templates/article.php');
 }
 else
--- index.php
+++ index.php
@@ -6,7 +6,7 @@ $query = 'SELECT * FROM news ORDER BY id DESC';
 
 $view = new View();
 $db = new DB;
-$view ->add('newslist', $db->query($query,[],\News\Article::class));
+$view->add('newslist', $db->query($query,[],\News\Article::class));
 
 
 $view->display(__DIR__ . '/templates/index.php');
